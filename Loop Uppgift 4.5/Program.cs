ecd1cbb1244ffd02ff78f3c605e3808c770d1e15
Anton <>
2023-03-10 09:27:13 +0100

    Add project files.

---
+++ Loop Uppgift 4.5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("skriv in ett heltal");
+            string heltal = Console.ReadLine();
+            
+           
+                for (int I = 0; I < heltal.Length; I++)
+                {
+                string talet = "";
+                    string tecken = heltal[I].ToString();
+                    int siffra = Convert.ToInt32(tecken);
+
+
+                    if (siffra == 9)
+                    {
+                        int tal = 0;
+                        Console.WriteLine(tal);
+                    }
+                    else
+                    {
+                        int tal = siffra + 1;
+                        Console.WriteLine(tal);
+                    }
+                }
+        }
+
+    }
+}
